72d135e53dfbcbbf8eac7f2de378986704af5c59
Zarni Htet <zhtet13@cornellcollege.edu>
2013-10-30 03:41:30 -0500

    Some more additions

--- challenge8.5.rb
+++ challenge8.5.rb
@@ -1,34 +1,50 @@
-Variable scope
+# encoding: UTF-8
 
-#a 
-z = [1, 2, 3, 5, 6,7]
+# Variable scope
 
-z = z.each do |l|
-  if l.even?
-    z.delete(l)
+# #a 
+# z = [1, 2, 3, 5, 6,7]
+
+# z = z.each do |l|
+#   if l.even?
+#     z.delete(l)
+#   end
+# end
+# puts z
+
+# #b
+# a = ['ZarNi', 'HtEt', 'ChrIs', 'TeA', 'LEaf']
+
+# a.each do |l|
+#   if l.is_a? String
+#      l.downcase
+#   end
+# end 
+# puts a
+
+# #c
+# a = ['ZarNi', 'HtEt', 'ChrIs', 'TeA', 'LEaf']
+
+# a.each do |l|
+# 	if l.is_a? String
+# 		 l.downcase!
+# 	end
+# end 
+# puts a
+
+# #2 do-end blocks
+# xorm = 'z'
+
+
+
+3.times do
+  chem = 'Phu'
+  puts chem
+  2.times do
+    xo = 'Nwe'
+    puts xo
   end
+  puts xo
 end
-puts z
-
-#b
-a = ['ZarNi', 'HtEt', 'ChrIs', 'TeA', 'LEaf']
-
-a.each do |l|
-  if l.is_a? String
-     l.downcase
-  end
-end 
-puts a
-
-#c
-a = ['ZarNi', 'HtEt', 'ChrIs', 'TeA', 'LEaf']
-
-a.each do |l|
-	if l.is_a? String
-		 l.downcase!
-	end
-end 
-puts a
-
-
+#puts chem
 
